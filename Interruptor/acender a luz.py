436f31620797e6062455955018a5b3fffb2f346e
Andrew Vinicius <74573141+andrewvininius@users.noreply.github.com>
2023-02-27 14:38:38 -0300

    Add files via upload

--- Interruptor/acender a luz.py
+++ Interruptor/acender a luz.py
@@ -2,7 +2,9 @@
 
 from tkinter import *
 
-from PIL import Image, ImageTk 
+from PIL import Image, ImageTk
+
+import random
 
 # cores 
 co0 = "#f0f3f5"  # cizenta / grey
@@ -15,7 +17,7 @@ janela = Tk()
 janela.title("")
 janela.geometry("400x260")
 janela.configure(background=co4)
-janela.resizable(width=FALSE, height=FALSE)
+janela.resizable(width=False, height=False)
 
 #criar frame
 frame_cima = Frame(janela, width=500, height=50, bg= co4)
@@ -55,6 +57,60 @@ l_img.place(x=30,y=10)
 l_estado = Label(frame_baixo, text='Está Escuro', anchor=NW, font=('Ivy 13'),bg=co4, fg=co1)
 l_estado.place(x=80,y=20)
 
+global control
+
+def ligar_lampada(i):
+    global control
+    lista = i
+    # desabilitando o botao que foi clicado
+    if lista[1] == 'interuptor - 1':
+        b_interruptor_1['state'] = 'disable'
+    elif lista[1] == 'interuptor - 2':
+        b_interruptor_2['state'] = 'disable'   
+    elif lista[1] == 'interuptor - 3':
+        b_interruptor_3['state'] = 'disable' 
+    elif lista[1] == 'interuptor - 4':
+        b_interruptor_4['state'] = 'disable' 
+    else: 
+        b_interruptor_5['state'] = 'disable'
+
+    def subistituir_valor(i): 
+        global control
+        nova_lista = []
+        for string in control:
+            novo_valor = string.replace(i[0],i[1]) 
+            nova_lista.append(novo_valor)   
+
+        control  = nova_lista           
+
+    #Valor aleatorio
+
+    valor_selecionado = random.sample(lista[0],1)[0]
+    if int(valor_selecionado) == 1:
+        if control[0] == 'lampada_1':
+            l_img_1['image'] = img_lampada_on
+            l_img['image'] = img_3
+            l_estado['text'] = 'Ok obrigado'
+            subistituir_valor(['lampada_1',str(1)])
+        else:
+            if control[1] == 'lampada_2':
+                l_img_2['image'] = img_lampada_on
+                l_img['image'] = img_4
+                l_estado['text'] = 'Por favor, acenda todas as luz'
+                subistituir_valor(['lampada_2',str(2)])
+            else:
+                if control[2] == 'lampada_3':
+                    l_img_3['image'] = img_lampada_on
+                    l_img['image'] = img_5
+                    l_estado['text'] = 'Muito Obrigado! '
+                    subistituir_valor(['lampada_3',str(3)])     
+            
+        
+
+
+
+control = ['lampada_1','lampada_2','lampada_3']
+
 #lampadas
 img_lampada_on = Image.open('1.png')
 img_lampada_on = img_lampada_on.resize((110,110))
@@ -66,30 +122,32 @@ img_lampada_off = ImageTk.PhotoImage(img_lampada_off)
 
 #lampadas apagadas
 
-l_img = Label(frame_baixo, image= img_lampada_off,bg=co4)
-l_img.place(x=5,y=70)
+l_img_1 = Label(frame_baixo, image= img_lampada_off,bg=co4)
+l_img_1.place(x=5,y=70)
 
-l_img = Label(frame_baixo, image= img_lampada_off,bg=co4)
-l_img.place(x=105,y=70)
+l_img_2 = Label(frame_baixo, image= img_lampada_off,bg=co4)
+l_img_2.place(x=105,y=70)
 
-l_img = Label(frame_baixo, image= img_lampada_off,bg=co4)
-l_img.place(x=205,y=70)
+l_img_3 = Label(frame_baixo, image= img_lampada_off,bg=co4)
+l_img_3.place(x=205,y=70)
+
+lista = [0,1,1,1,0]
 
 #criando botao
 
-b_interruptor_1 = Button(frame_baixo, text='Interruptor- 1', anchor=NW, font=('Ivy 9 bold'),relief=RAISED, overrelief=RIDGE, bg=co4, fg=co1)
+b_interruptor_1 = Button(frame_baixo, command=lambda i=[lista,'interuptor - 1']: ligar_lampada(i),text='Interruptor- 1', anchor=NW, font=('Ivy 9 bold'),relief=RAISED, overrelief=RIDGE, bg=co4, fg=co1)
 b_interruptor_1.place(x=300,y=50)
 
-b_interruptor_2 = Button(frame_baixo, text='Interruptor- 2', anchor=NW, font=('Ivy 9 bold'),relief=RAISED, overrelief=RIDGE, bg=co4, fg=co1)
+b_interruptor_2 = Button(frame_baixo, command=lambda i=[lista,'interuptor - 2']: ligar_lampada(i), text='Interruptor- 2', anchor=NW, font=('Ivy 9 bold'),relief=RAISED, overrelief=RIDGE, bg=co4, fg=co1)
 b_interruptor_2.place(x=300,y=80)
 
-b_interruptor_3 = Button(frame_baixo, text='Interruptor- 3', anchor=NW, font=('Ivy 9 bold'),relief=RAISED, overrelief=RIDGE, bg=co4, fg=co1)
+b_interruptor_3 = Button(frame_baixo, command=lambda i=[lista,'interuptor - 3']: ligar_lampada(i), text='Interruptor- 3', anchor=NW, font=('Ivy 9 bold'),relief=RAISED, overrelief=RIDGE, bg=co4, fg=co1)
 b_interruptor_3.place(x=300,y=110)
 
-b_interruptor_4 = Button(frame_baixo, text='Interruptor- 4', anchor=NW, font=('Ivy 9 bold'),relief=RAISED, overrelief=RIDGE, bg=co4, fg=co1)
+b_interruptor_4 = Button(frame_baixo, command=lambda i=[lista,'interuptor - 4']: ligar_lampada(i),text='Interruptor- 4', anchor=NW, font=('Ivy 9 bold'),relief=RAISED, overrelief=RIDGE, bg=co4, fg=co1)
 b_interruptor_4.place(x=300,y=140)
 
-b_interruptor_5 = Button(frame_baixo, text='Interruptor- 5', anchor=NW, font=('Ivy 9 bold'),relief=RAISED, overrelief=RIDGE, bg=co4, fg=co1)
+b_interruptor_5 = Button(frame_baixo, command=lambda i=[lista,'interuptor - 5']: ligar_lampada(i),text='Interruptor- 5', anchor=NW, font=('Ivy 9 bold'),relief=RAISED, overrelief=RIDGE, bg=co4, fg=co1)
 b_interruptor_5.place(x=300,y=170)
 
 
